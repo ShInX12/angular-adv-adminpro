b445080db6252b81b2541e69807cb26f43493add
Sergio Naranjo <sergio6006@hotmail.com>
2020-08-06 16:07:10 -0500

    Environment de producción listo

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanLoad, Route, UrlSegment} from '@angular/router';
 import {UsuarioService} from '../services/usuario.service';
 import {tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private usuarioService: UsuarioService,
               private router: Router) {
@@ -16,6 +17,19 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
 
+    return this.usuarioService.validarToken()
+      .pipe(
+        tap(estaAutenticado => {
+          if (!estaAutenticado){
+            this.router.navigateByUrl('/login');
+          }
+        })
+      );
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    // Debe retornar una promesa que resulva true o false
     return this.usuarioService.validarToken()
       .pipe(
         tap(estaAutenticado => {
